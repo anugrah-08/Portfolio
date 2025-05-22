9dccd49b49bd64e08746aef9c1fabafa96667836
anugrah-08 <thisanugrahem@gmail.com>
2025-05-22 13:48:51 +0530

    added github and linkedin in contact section

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -1,9 +1,14 @@
-import React, { useRef } from 'react'
-import './contact.css'
+import React, { useRef } from 'react';
+import './contact.css';
 import emailjs from '@emailjs/browser';
 
 const Contact = () => {
+  const alertBox = () => {
+    alert("Mail is under maintenance, please contact me through LinkedIn or Github");
+  };
+
   const form = useRef();
+
   const sendEmail = (e) => {
     e.preventDefault();
 
@@ -14,30 +19,45 @@ const Contact = () => {
       .then(
         () => {
           console.log('SUCCESS!');
-          form.current.reset()
+          form.current.reset();
         },
         (error) => {
           console.log('FAILED...', error.text);
-        },
+        }
       );
   };
 
-
   return (
     <div id="mycontact">
-        <div className='contact'>
-            <h1 className="head">Contact Me</h1>
-            <form ref={form} onSubmit={sendEmail}>
-                <input type="text" className="name" placeholder='Your Name' name='from_name' required />
-                <input type="email" className="email" placeholder='Your Email' name='from_email' required />
-                <textarea className="message" rows={"5"} placeholder='Your Message' name='message' required />
-                <button className="send" type='submit'>Submit</button>
-            </form>
-
-        
-        </div>
+      <div className="contact">
+        <h1 className="head">Contact Me</h1>
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" className="name" placeholder="Your Name" name="from_name" />
+          <input type="email" className="email" placeholder="Your Email" name="from_email" />
+          <textarea className="message" rows="5" placeholder="Your Message" name="message" disabled />
+          <button className="send" type="submit" onClick={alertBox}>
+            Submit
+          </button>
+          <div className="social">
+            <a className='icons'
+              href="https://www.linkedin.com/in/anugrah-e-m-8002b827b"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="fa-brands fa-linkedin"></i>
+            </a>
+            <a className='icons'
+              href="https://github.com/anugrah-08"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="fa-brands fa-github"></i>
+            </a>
+          </div>
+        </form>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Contact;
